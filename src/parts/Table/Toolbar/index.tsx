2fd7cc71c86fc718fc3f5e32e33c3aa40d10e424
Jeanne4L <sandra.petereau@outlook.fr>
2025-05-11 07:28:12 +0200

    feat(table): create stories + fix style

--- src/parts/Table/Toolbar/index.tsx
+++ src/parts/Table/Toolbar/index.tsx
@@ -4,15 +4,17 @@ import { ToolbarContext } from "../../../context/Toolbar"
 import { useTable } from "../../../context/Table"
 import { ActionsContainer } from "./styles"
 
+const value = { isInsideToolbar: true }
+
 const Toolbar: React.FC<PropsWithChildren> = ({ children }) => {
   const { isInsideTable } = useTable()
 
   if(!isInsideTable) {
     throw new Error('Table.Toolbar must be inside Table')
   }
-  
+
   return (
-    <ToolbarContext.Provider value={{ isInsideToolbar: true }}>
+    <ToolbarContext.Provider value={value}>
       <ActionsContainer>
         {children}
       </ActionsContainer>
