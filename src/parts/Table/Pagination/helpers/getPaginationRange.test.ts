ebe1e07ea9fccfc589f7db77500fe0d73c2b6166
Jeanne4L <sandra.petereau@outlook.fr>
2025-05-23 10:10:22 +0200

    refactor(getPaginationRange): decompose helper test

--- src/parts/Table/Pagination/helpers/getPaginationRange.test.ts
+++ src/parts/Table/Pagination/helpers/getPaginationRange.test.ts
@@ -2,14 +2,29 @@ import { getPaginationRange } from "./getPaginationRange"
 
 describe('helpers', () => {
   test('return pagination range at start', () => {
-    expect(getPaginationRange(10, 1)).toEqual([1, 2, 3, 4, 5, 'dots', 10])
+    const itemsNumber = 10
+    const currentPage = 1
+
+    const result = getPaginationRange(itemsNumber, currentPage)
+
+    expect(result).toEqual([1, 2, 3, 4, 5, 'dots', 10])
   })
 
   test('return pagination range at end', () => {
-    expect(getPaginationRange(10, 10)).toEqual([1, 'dots', 6, 7, 8, 9, 10])
+    const itemsNumber = 10
+    const currentPage = 10
+
+    const result = getPaginationRange(itemsNumber, currentPage)
+
+    expect(result).toEqual([1, 'dots', 6, 7, 8, 9, 10])
   })
 
   test('return pagination range in middle', () => {
-    expect(getPaginationRange(15, 8)).toEqual([1, 'dots', 7, 8, 9, 'dots', 15])
+    const itemsNumber = 15
+    const currentPage = 8
+
+    const result = getPaginationRange(itemsNumber, currentPage)
+
+    expect(result).toEqual([1, 'dots', 7, 8, 9, 'dots', 15])
   })
 })
